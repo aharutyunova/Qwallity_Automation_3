533d005039a0ab331d305c750f3f92b350a91e33
aharutyunova <55526198+aharutyunova@users.noreply.github.com>
2023-07-24 23:19:23 +0400

    Update environment.py

--- Helpers/environment.py
+++ Helpers/environment.py
@@ -8,3 +8,4 @@ config_data_for_second_user_draft = {
     'email': 'qwallityautomation3@gmail.com',
     'password': '13072022'
 }
+# Anna -environment.py file will be better to take outside of Helpers folder
